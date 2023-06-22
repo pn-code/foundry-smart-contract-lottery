df0a538d9a5ecb409e43adff079cd3bd33b02899
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-22 15:26:06 -0700

    Added tests for enterRaffle function

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -20,6 +20,12 @@ contract RaffleTest is Test {
     address public PLAYER = address(1);
     uint256 public constant STARTING_USER_BALANCE = 10 ether;
 
+    /**
+    * Events
+    */
+    event EnteredRaffle(address indexed player);
+    event PickedWinner(address indexed winner);
+
     function setUp() external {
         DeployRaffle deployer = new DeployRaffle();
         (raffle, helperConfig) = deployer.run();
@@ -27,9 +33,37 @@ contract RaffleTest is Test {
         // Deconstructing properties from our active config...
         (entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit) =
             helperConfig.activeNetworkConfig();
+
+        vm.deal(PLAYER, STARTING_USER_BALANCE);
     }
 
     function testRaffleInitializesInOpenState() public view {
-        assertEq(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+        assert(raffle.getRaffleState() == Raffle.RaffleState.OPEN);
+    }
+
+    /**
+     * enterRaffle
+     */
+
+    function testRaffleRevertsWhenYouDontPayEnough() public {
+        vm.prank(PLAYER);
+
+        vm.expectRevert(Raffle.Raffle__NotEnoughETH.selector);
+        raffle.enterRaffle();
+    }
+
+    function testRaffleRecordsPlayerWhenTheyEnter() public {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        address playerRecorded = raffle.getPlayer(0);
+        assertEq(playerRecorded, PLAYER);
+    }
+
+    function testRaffleToEmitEnteredRaffleEvent() public {
+        vm.prank(PLAYER);
+        vm.expectEmit(true, false, false, false, address(raffle));
+
+        emit EnteredRaffle(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
     }
 }
