6f9c2d4f4a6a3c986f67e9aab17f28c26ed46631
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-22 13:56:59 -0700

    Created DeployRaffle script

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/src/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {
+        HelperConfig helperConfig = new HelperConfig();
+
+        // Deconstructing properties from our active config...
+        (
+            uint256 entranceFee,
+            uint256 interval,
+            address vrfCoordinator,
+            bytes32 gasLane,
+            uint64 subscriptionId,
+            uint32 callbackGasLimit
+        ) = helperConfig.activeNetworkConfig();
+
+        vm.startBroadcast();
+        Raffle raffle = new Raffle(entranceFee, interval, vrfCoordinator, gasLane, subscriptionId, callbackGasLimit);
+        vm.stopBroadcast();
+
+        return raffle;
+    }
+}
