bb472ae6049de8b3522fecc6582972b7bded205d
Philip Nguyen <philipnguyen953@gmail.com>
2023-06-22 17:41:41 -0700

    Completed fundSubscription script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import {Script, console} from "forge-std/src/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
 
 contract CreateSubscription is Script {
     function createSubscriptionUsingConfig() public returns (uint64) {
@@ -32,12 +33,29 @@ contract CreateSubscription is Script {
 contract FundSubscription is Script {
     uint96 public constant FUND_AMOUNT = 3 ether;
 
-    function funSubscriptionUsingConfig() public {
+    function fundSubscriptionUsingConfig() public {
         HelperConfig helperConfig = new HelperConfig();
         (,, address vrfCoordinator,, uint64 subscriptionId,, address link) = helperConfig.activeNetworkConfig();
     }
 
+    function fundSubscription(address vrfCoordinator, uint64 subscriptionId, address link) public {
+        console.log("Funding subscription: ", subscriptionId);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
+        console.log("On ChainID: ", block.chainid);
+
+        // If on anvil
+        if(block.chainid == 31337) {
+            vm.startBroadcast();
+            VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(subscriptionId, FUND_AMOUNT);
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast();
+            LinkToken(link).transferAndCall(vrfCoordinator, FUND_AMOUNT, abi.encode(subscriptionId));
+            vm.stopBroadcast();
+        }
+    }
+
     function run() external {
-        funSubscriptionUsingConfig();
+        fundSubscriptionUsingConfig();
     }
 }
